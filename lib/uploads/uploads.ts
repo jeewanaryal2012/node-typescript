08d4db8c13ac5775579d9f45cbbc9287c85f5f65
jeewanaryal <jeewanaryal@gmail.com>
2020-05-07 12:40:20 -0500

    profile pic

--- lib/uploads/uploads.ts
+++ lib/uploads/uploads.ts
@@ -2,6 +2,7 @@ import * as multer from 'multer'
 import * as fs from 'fs';
 import db from '../db/connect';
 import * as path from 'path';
+import * as findRemoveSync from 'find-remove';
 
 const PATH = './uploads/tmp';
 
@@ -9,6 +10,7 @@ export default class Uploads {
     storage: any;
     upload: any;
     connection: any;
+    finalFileName: any;
     constructor() {
         this.storage = multer.diskStorage({
             destination: (req, file, cb) => {
@@ -38,8 +40,8 @@ export default class Uploads {
                 //console.log(fileName, fileExtention, req.headers);
                 // let fileName = `${file.originalname}${Math.floor(Math.random() * 1000000)}`;
                 //Math.floor(Math.random() * 1000000);
-                const finalFileName = `profile-${userEmail}-${dateNow}.${fileExtention}`;
-                cb(null, finalFileName);
+                this.finalFileName = `profile-${userEmail}-${dateNow}.${fileExtention}`;
+                cb(null, this.finalFileName);
             }
         });
         this.upload = multer({
@@ -74,31 +76,43 @@ export default class Uploads {
     }
 
     uploadProfilePicture(req, res) {
+        console.log(this.finalFileName);
         const userEmail = req.headers['email'];
         fs.readdir(path.join(process.env.PWD, './uploads/tmp'), (err, files) => {
             files.forEach((file, index) => {
                 if (file === userEmail) {
                     fs.readdir(path.join(process.env.PWD, './uploads/tmp/' + file), (err, f) => {
-                        //res.sendFile(process.env.PWD + '/uploads/tmp/' + file + '/' + f[0]);
-                        console.log('----');
-                        console.log(f);
-                        f.forEach((item, index) => {
-                            //console.log(process.env.PWD + './uploads/tmp/' + file);
-                            if (item === '10.jpeg') {
-                                fs.unlinkSync(process.env.PWD + '/uploads/tmp/' + file + '/10.jpeg');
-                            }
-                        });
+                        // console.log('----');
+                        //console.log(f);
+                        // https://www.npmjs.com/package/find-remove
+                        //findRemoveSync(process.env.PWD + '/uploads/tmp/' + file, { files: ['1.jpeg'], ignore: 'haumiblau.bak' });
+                        //findRemoveSync(process.env.PWD + '/uploads/tmp/' + file, { extensions: ['.jpeg'], ignore: 'profile-kate@gmail.com-1588867951596.jpeg' })
+                        const sortedFiles = this.sortUtil(f);
+                        const recentUpload = sortedFiles[sortedFiles.length - 1];
+                        console.log('recent: ', recentUpload);
+                        //findRemoveSync(process.env.PWD + '/uploads/tmp/' + file, { dir: "*", files: "*.*", ignore: this.recentUpload() });
                         let max = f.length - 1, min = 1;
                         let ran = Math.floor(Math.random() * (max - min + 1)) + min;
-                        //const fName = f[0] === '.DS_Store' ? f[1] : f[0];
                         const fName = f[f.length - 2];
+                        const profilePicture = 'http://localhost:4040' + '/uploads/tmp/' + file + '/' + recentUpload;
                         res.json({
-                            profilePicture: 'http://localhost:4040' + '/uploads/tmp/' + file + '/' + fName
+                            profilePicture: profilePicture
                         });
+                        findRemoveSync(process.env.PWD + '/uploads/tmp/' + file, { dir: "*", files: "*.*", ignore: recentUpload });
+                        this.connection.query(`UPDATE userProfile SET profilePicture = ? WHERE email = ?`, [profilePicture, userEmail], (error, results, fields) => { });
                     });
                 }
             });
         });
+    }
+
+    sortUtil(f) {
+        const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' });
+        const sorted = f.sort(collator.compare);
+        console.log(f);
+        console.log(sorted);
+        return sorted;
+        //return 'profile-kate@gmail.com-1588869488724.jpeg';
     }
 }
 
