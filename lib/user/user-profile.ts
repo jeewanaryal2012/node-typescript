1d4ac8bfa3c35852fa05398523af2497e67301a0
jeewanaryal <jeewanaryal@gmail.com>
2020-05-08 11:04:13 -0500

    wip

--- lib/user/user-profile.ts
+++ lib/user/user-profile.ts
@@ -43,33 +43,75 @@ export default class UserProfile {
             // const query = `SELECT firstName, lastName, users.email, gender, isMember, lastLogin, profilePicture FROM users
             //                 INNER JOIN userProfile WHERE users.email = userProfile.email
             // `;
-            const query = `SELECT u.firstName, u.lastName, u.email, u.gender, u.isMember, u.lastLogin, u.uuid, up.profilePicture, fl.uuidA, fl.uuidB, fl.status
+            const query = `SELECT u.firstName, u.lastName, u.email, u.gender, u.isMember, u.lastLogin, u.uuid, up.profilePicture
                             FROM users u
-                            INNER JOIN userProfile up USING (email)
-                            INNER JOIN friendList fl ON fl.email = 'jeewan@gmail.com';`;
+                            INNER JOIN userProfile up USING (email)`;
 
             this.connection.query(query, (err, rows) => {
                 if (err) {
                     throw err;
                 } else {
                     console.log(rows.length);
-                    let filtered = rows.filter((item, index) => {
-                        return item.email !== currentUser;
+                    // let filtered = rows.filter((item, index) => {
+                    //     return item.email !== currentUser;
+                    // });
+
+                    // const filteredArr = filtered.reduce((acc, current) => {
+                    //     const x = acc.find(item => item.email === current.email);
+                    //     if (!x) {
+                    //         return acc.concat([current]);
+                    //     } else {
+                    //         return acc;
+                    //     }
+                    // }, []);
+
+                    const friendListQuery = `SELECT * FROM friendList`;
+                    this.connection.query(friendListQuery, (err, flRow) => {
+                        const mapped = this.mapUserFriendList(rows, flRow, currentUser);
+                        subscriber.next(mapped);
                     });
 
-                    const filteredArr = filtered.reduce((acc, current) => {
-                        const x = acc.find(item => item.email === current.email);
-                        if (!x) {
-                            return acc.concat([current]);
-                        } else {
-                            return acc;
-                        }
-                    }, []);
-
-                    subscriber.next(filteredArr);
                 }
             });
         });
         return observable;
+    } filteredArr
+
+    mapUserFriendList(arr, fList, currentUser) {
+        console.log(fList);
+        let result = [];
+        arr.forEach((el1, idx1) => {
+            fList.forEach((el2, idx2) => {
+                result.push({
+                    firstName: el1.firstName,
+                    lastName: el1.lastName,
+                    email: el1.email,
+                    gender: el1.gender,
+                    ismember: el1.isMember,
+                    lastLogin: el1.lastLogin,
+                    uuid: el2.uuidB,
+                    profilePicture: el1.profilePicture,
+                    uuidB: el2.uuidB,
+                    status: el1.uuid === el2.uuidB ? el2.status : 'n'
+
+                }
+                );
+                // if (el1.uuid === el2.uuidB) {
+
+                // }
+            });
+        });
+        let filtered = result.filter((item, index) => {
+            return item.email !== currentUser;
+        });
+        const filteredArr = filtered.reduce((acc, current) => {
+            const x = acc.find(item => item.email === current.email);
+            if (!x) {
+                return acc.concat([current]);
+            } else {
+                return acc;
+            }
+        }, []);
+        return filteredArr;
     }
 }
